d3704f1c92a0ac18b967d504a9dfd89cc9008c35
ikiruhawk <ikiruhawk@gmail.com>
2024-07-21 02:48:20 +0400

    upd routers

--- internal/routers/routers.go
+++ internal/routers/routers.go
@@ -9,15 +9,17 @@ import (
 
 func RunRouters() *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/", homeHandleFunc)
-	r.HandleFunc("/products", productsHandleFunc)
+	r.HandleFunc("/", homeHandleFunc).Methods("GET")
+	r.HandleFunc("/products", productsHandleFunc).Methods("GET")
 	return r
 }
 
 func homeHandleFunc(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 	fmt.Fprint(w, "Home Page")
 }
 
 func productsHandleFunc(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 	fmt.Fprint(w, "List of products")
 }
